69c0733d9203cc2a3a771cf33e1a7ac3896f85c0
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-09 11:08:24 -0300

    tables ok

--- src/main/java/cantina/connections/tables.sql
+++ src/main/java/cantina/connections/tables.sql
@@ -1,12 +1,41 @@
 CREATE DATABASE IF NOT EXISTS cantina;
 USE cantina;
-create table IF NOT EXISTS funcionario (
-    nome varchar(50),
-    email varchar(50),
-    senha varchar(50)
+CREATE TABLE IF NOT EXISTS funcionario (
+    id INT(8) AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(256) NOT NULL,
+    senha VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
-create table IF NOT EXISTS produto (
-    nome varchar(50),
-    preco Decimal(14,2),
-    descricao varchar(50)
+CREATE TABLE IF NOT EXISTS produto (
+    codigo INT(8) AUTO_INCREMENT,
+    id_funcionario INT(8),
+    nome VARCHAR(100) NOT NULL,
+    preco_compra Decimal(14,2) NOT NULL,
+    preco_venda Decimal(14,2) NOT NULL,
+    quantidade_comprada INT(7) NOT NULL,
+    quantidade_vendida INT(7) NOT NULL,
+    quantidade_atual INT(7) NOT NULL,
+    PRIMARY KEY (codigo),
+    FOREIGN KEY (id_funcionario) REFERENCES funcionario(id)
+);
+CREATE TABLE IF NOT EXISTS venda (
+    codigo INT(8) AUTO_INCREMENT,
+    id_funcionario INT(8) NOT NULL,
+    desconto Decimal(14,2) NOT NULL,
+    total_venda Decimal(14,2) NOT NULL,
+    forma_pagamento VARCHAR(25),
+    data DATE,
+    PRIMARY KEY (codigo),
+    FOREIGN KEY (id_funcionario) REFERENCES funcionario(id)
+);
+CREATE TABLE IF NOT EXISTS item_venda (
+    codigo_item INT(8) AUTO_INCREMENT,
+    codigo_produto INT(8),
+    codigo_venda INT(8),
+    preco_venda Decimal(14,2),
+    quantidade INT(8) NOT NULL,
+    PRIMARY KEY (codigo_item),
+    FOREIGN KEY (codigo_produto) REFERENCES produto(codigo),
+    FOREIGN KEY (codigo_venda) REFERENCES venda(codigo)
 );
