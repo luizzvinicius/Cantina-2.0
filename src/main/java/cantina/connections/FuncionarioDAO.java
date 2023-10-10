64fe24b140ed5e404787db89375c61395df9a1f6
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-10 11:30:14 -0300

    add buscaFuncionario

--- src/main/java/cantina/connections/FuncionarioDAO.java
+++ src/main/java/cantina/connections/FuncionarioDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 import cantina.Funcionario;
 
@@ -14,24 +16,44 @@ public class FuncionarioDAO {
         this.conn = conn;
     }
 
-    public void insert(Funcionario func) {
-        final String sql = "insert into funcionario (nome) values (?)";
+    public int insert(Funcionario func) {
+        final String sql = "insert into funcionario (nome, email, senha) values (?, ?, ?)";
+        int rows = 0;
         try (var stmt = this.conn.prepareStatement(sql)) {
             stmt.setString(1, func.getNome());
-            stmt.execute();
+            stmt.setString(2, func.getEmail());
+            stmt.setString(3, func.getSenha());
+            rows = stmt.executeUpdate();
             System.out.println("Funcionário inserido.");
         } catch (Exception e) {
-            System.out.println("Erro ao cadastrar funcionário: " + e.getMessage());
+            System.out.println("Erro ao cadastrar funcionário: " + e.getMessage() + "\n");
         }
+        return rows;
     }
 
     public List<Funcionario> select() {
-        final String sql = "select nome, email from funcionario";
+        final String sql = "select id, nome, email from funcionario";
         List<Funcionario> funcionarios = new ArrayList<>();
         try (var stmt = this.conn.prepareStatement(sql)) {
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
-                funcionarios.add(new Funcionario(rs.getString("nome"), rs.getString("email")));
+                funcionarios.add(new Funcionario(rs.getInt("id"), rs.getString("nome"), rs.getString("email"), null));
+            }
+        } catch (Exception e) {
+            System.out.println("Erro ao selecionar funcionário(s): " + e.getMessage());
+        }
+        return funcionarios;
+    }
+
+    public Map<Integer, String> buscaFuncionario(String email, String senha) {
+        final String sql = "select id, nome from funcionario where email = ? and senha = ?";
+        Map<Integer, String> funcionarios = new HashMap<>();
+        try (var stmt = this.conn.prepareStatement(sql)) {
+            stmt.setString(1, email);
+            stmt.setString(2, senha);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                funcionarios.put(rs.getInt("id"), rs.getString("nome"));
             }
         } catch (Exception e) {
             System.out.println("Erro ao selecionar funcionário(s): " + e.getMessage());
