f1a65391b9071ecbe6d8ac0f63f484c8ed02290c
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-10 11:28:37 -0300

    add constraint

--- src/main/java/cantina/connections/tables.sql
+++ src/main/java/cantina/connections/tables.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS funcionario (
 CREATE TABLE IF NOT EXISTS produto (
     codigo INT(8) AUTO_INCREMENT,
     id_funcionario INT(8),
-    nome VARCHAR(100) NOT NULL,
+    nome VARCHAR(100) NOT NULL UNIQUE,
     preco_compra Decimal(14,2) NOT NULL,
     preco_venda Decimal(14,2) NOT NULL,
     quantidade_comprada INT(7) NOT NULL,
@@ -19,6 +19,7 @@ CREATE TABLE IF NOT EXISTS produto (
     PRIMARY KEY (codigo),
     FOREIGN KEY (id_funcionario) REFERENCES funcionario(id)
 );
+ALTER TABLE produto ADD CHECK (preco_venda > preco_compra);
 CREATE TABLE IF NOT EXISTS venda (
     codigo INT(8) AUTO_INCREMENT,
     id_funcionario INT(8) NOT NULL,
