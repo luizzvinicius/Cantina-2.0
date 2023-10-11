1e4fe3e3496bb5bdb9db2c469f120b245dcc9e8b
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-11 11:32:07 -0300

    refactor tables

--- src/main/java/cantina/connections/tables.sql
+++ src/main/java/cantina/connections/tables.sql
@@ -3,7 +3,7 @@ USE cantina;
 CREATE TABLE IF NOT EXISTS funcionario (
     id INT(8) AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
-    email VARCHAR(256) NOT NULL UNIQUE,
+    email VARCHAR(256) UNIQUE,
     senha VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
@@ -16,17 +16,22 @@ CREATE TABLE IF NOT EXISTS produto (
     quantidade_comprada Decimal(14,2) NOT NULL,
     quantidade_vendida Decimal(14,2) NOT NULL,
     quantidade_atual Decimal(14,2) NOT NULL,
+    CHECK (preco_compra >= 0),
+    CHECK (preco_venda >= 0),
+    CHECK (quantidade_comprada >= 0),
+    CHECK (preco_venda > preco_compra),
     PRIMARY KEY (codigo),
     FOREIGN KEY (id_funcionario) REFERENCES funcionario(id)
 );
-ALTER TABLE produto ADD CHECK (preco_venda > preco_compra);
 CREATE TABLE IF NOT EXISTS venda (
     codigo INT(8) AUTO_INCREMENT,
     id_funcionario INT(8) NOT NULL,
     desconto Decimal(14,2) NOT NULL,
     total_venda Decimal(14,2) NOT NULL,
-    forma_pagamento VARCHAR(25),
-    data DATE,
+    forma_pagamento VARCHAR(25) NOT NULL,
+    data DATE NOT NULL,
+    CHECK (desconto >= 0),
+    CHECK (total_venda > 0),
     PRIMARY KEY (codigo),
     FOREIGN KEY (id_funcionario) REFERENCES funcionario(id)
 );
@@ -34,9 +39,11 @@ CREATE TABLE IF NOT EXISTS item_venda (
     codigo_item INT(8) AUTO_INCREMENT,
     codigo_produto INT(8),
     codigo_venda INT(8),
-    preco_venda Decimal(14,2),
+    preco_venda Decimal(14,2) NOT NULL,
     quantidade Decimal(14,2) NOT NULL,
+    CHECK (preco_venda > 0),
+    CHECK (quantidade > 0),
     PRIMARY KEY (codigo_item),
-    FOREIGN KEY (codigo_produto) REFERENCES produto(codigo),
+    FOREIGN KEY (codigo_produto) REFERENCES produto(codigo) ON DELETE CASCADE,
     FOREIGN KEY (codigo_venda) REFERENCES venda(codigo)
 );
