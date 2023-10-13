e49a59a18a87ba0ab084c15a08500059e03af5cb
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-13 08:11:41 -0300

    refactor

--- src/main/java/cantina/validacao/Entrada.java
+++ src/main/java/cantina/validacao/Entrada.java
@@ -12,10 +12,9 @@ public class Entrada implements AutoCloseable {
 
     public String lerString(String msg, String errorMessage) {
         var scan = System.console();
-        String palavra;
         while (true) {
             System.out.print(msg);
-            palavra = scan.readLine().strip().toLowerCase();
+            var palavra = scan.readLine().strip().toLowerCase();
             if (!Pattern.matches("^[a-zÀ-ÿ\s]+$", palavra)) {
                 System.out.println(errorMessage + "\n");
                 continue;
@@ -26,12 +25,11 @@ public class Entrada implements AutoCloseable {
 
     public String lerEmail(String msg) {
         var scan = System.console();
-        String email;
         while (true) {
             System.out.print(msg);
-            email = scan.readLine().strip().toLowerCase();
-            if (!Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$", email)) {
-                System.out.println("Formato de Email inválido" + "\n");
+            String email = scan.readLine().strip().toLowerCase();
+            if (!Pattern.matches("^[a-z0-9+_.-]+@(.+)$", email)) {
+                System.out.printf("Formato de email inválido.%n%n");
                 continue;
             }
             return email;
@@ -72,9 +70,8 @@ public class Entrada implements AutoCloseable {
     }
 
     public int lerOption(String msg, int min, int max, String erroMessage) {
-        int num;
         while (true) {
-            num = this.lerInt(msg);
+            var num = this.lerInt(msg);
             try {
                 if (num < min || num > max) {
                     throw new IllegalArgumentException(erroMessage + "\n");
